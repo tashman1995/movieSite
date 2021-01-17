0c8204ed96774defb8c4a7a16872d206919d35dc
tashman1995 <tom.ashman1995@gmail.com>
2021-01-17 15:46:26 +0000

    app.use changed

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 var express = require("express");
 var app = express();
+var fs = require("fs"),
+  express = require("express");
 
-app.use(express.static("public"));
+app.use(express.static(__dirname + "/public"));
 
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/" + "index.html");
+  fs.readFile(__dirname + "/public/index.html", "utf8", function (err, text) {
+    res.send(text);
+  });
 });
 
 const PORT = process.env.PORT || 5000;
